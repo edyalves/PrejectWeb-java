75e31deb072807031b6e42ecd16bee50a1ff0e6a
Edy Alves <edy4alves@gmail.com>
2021-05-29 18:58:05 -0300

    Project created

---
+++ src/test/java/com/projectWeb/course/ProjectWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectWeb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
