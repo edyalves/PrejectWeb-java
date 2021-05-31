3b4ae90285d0d5b730fc9f0ea8a9c11c0a5e99c0
Edy Alves <edy4alves@gmail.com>
2021-05-31 18:49:17 -0300

    User entities and resources

---
+++ src/main/java/com/projectWeb/course/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.projectWeb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projectWeb.course.entities.User;
+
+//RECURSO WEB IMPREMENTANDO POR UM CONTROLADOR REST
+//NOME DO RECURSO
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(2L, "Edy", "edy@gmail.com", "9999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+
+}
