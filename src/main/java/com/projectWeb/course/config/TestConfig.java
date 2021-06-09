091c1e13feb6f554ac5db9984e1b2655960e96dc
Edy Alves <edy4alves@gmail.com>
2021-06-08 21:16:44 -0300

    User insert

--- src/main/java/com/projectWeb/course/config/TestConfig.java
+++ src/main/java/com/projectWeb/course/config/TestConfig.java
@@ -44,7 +44,8 @@ public class TestConfig implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
 		
 		Order o1 = new Order(null, Instant.parse("2099-06-20T00:00:05Z"), OrderStatus.WAITTING_PAYMENT, u1);
 		Order o2 = new Order(null, Instant.parse("2150-06-20T00:00:05Z"), OrderStatus.PAID, u2);
